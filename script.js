05a41b87096802a5ec0fda5af5d0ada59f3e239c
Kivans <39023504+Kivans@users.noreply.github.com>
2018-10-15 16:37:31 +0200

    applied beautifier.io and some minor changes

--- script.js
+++ script.js
@@ -1,6 +1,9 @@
+//This program takes your shift starting time, ending time and day of the week as input
+//And returns Your monthly gross wage as output, including average daily salary,
+//Overtime hours, and how much these overtime hours yielded.
+
 //Converts the minutes to decimal value, i.e: 16:30 -> 16.5, 16:05 -> 16.08
-function minutesToDecimal(minutes)
-{
+function minutesToDecimal(minutes) {
   //Divide by 60 in order to make time decimal and remove any periods.
   let decimal = String(minutes / 60).replace(`.`, ``);
   //If the number is bigger than 0.1, remove the zero, i.e: 07 -> 7
@@ -11,10 +14,9 @@ function minutesToDecimal(minutes)
 }
 
 //Take shift start time and ending time as input, return wage as output
-function calculateWage(arr)
-{
+function calculateWage(arr) {
   let wage = 32;
-  let overtime = 8;//After 8 hours of work it is considered overtime, 7 on friday
+  let overtime = 8; //After 8 hours of work it is considered overtime, 7 on friday
   let pay = 0;
   let averagePay = 0;
   let weeklyHours = 0;
@@ -24,8 +26,7 @@ function calculateWage(arr)
   let totalOvertimePay = 0;
   let count = 0;
 
-  for (let day of arr)
-  {
+  for (let day of arr) {
     count++;
     //Take the input and make an array out of it, [0] = Hour, [1] = Minutes
     let end = day.End.split(":");
@@ -36,11 +37,11 @@ function calculateWage(arr)
     let shiftStart = Number(start[0] + "." + startMinutes);
     let shiftEnd = Number(end[0] + "." + endMinutes);
 
-    let hoursWorked = shiftEnd - shiftStart - 0.5;//deduct 0.5 for lunch break
+    let hoursWorked = shiftEnd - shiftStart - 0.5; //deduct 0.5 for lunch break
     totalHoursWorked += hoursWorked;
     weeklyHours += hoursWorked;
 
-    if (day.Day == "Friday"){
+    if (day.Day == "Friday") {
       overtime = 7;
     }
     else {
@@ -50,20 +51,17 @@ function calculateWage(arr)
     let overtimeHours = hoursWorked - overtime;
     let overtimePay = 0;
 
-
-
-    if (weeklyHours > weeklyHoursLimit){
+    if (weeklyHours > weeklyHoursLimit) {
       overtimeHours = weeklyHours - weeklyHoursLimit;
     }
 
     //Overtime system
-    if (overtimeHours > 0)
-    {
+    if (overtimeHours > 0) {
       let extraOvertime = 0;
       let extra = false;
 
       //150% Overtime system - if worked over 10 hours
-      if (hoursWorked > 10){
+      if (hoursWorked > 10) {
         extra = true;
         extraOvertime = hoursWorked - 10;
         overtimePay += extraOvertime * wage * 1.5;
@@ -85,29 +83,27 @@ function calculateWage(arr)
     averagePay += dailyPay;
 
     //End of week
-    if (day.Day == "Friday"){
+    if (day.Day == "Friday") {
       console.log("That day was a friday.");
       weeklyHours = 0;
     }
   }
   let msg = `You have worked ${totalHoursWorked.toFixed(1)} hours and earned a total of ${Math.round(pay)} shekels.`;
   msg += `<br>Your average pay is ${(averagePay/count).toFixed(0)} shekels per day.`;
-  if (totalOvertimePay > 0)
-  {
+  if (totalOvertimePay > 0) {
     msg += `<br>That includes ${totalOvertimeHours.toFixed(1)} hours overtime that yielded ${Math.round(totalOvertimeHours*wage*1.25 - totalOvertimeHours*wage)} extra shekels.`;
 
     return msg;
   }
 }
 
-function WorkDay(start, end, day)
-{
+function WorkDay(start, end, day) {
   this.Start = start;
   this.End = end;
   this.Day = day;
 }
 
-//October hours
+//September hours
 let day1 = new WorkDay("06:45", "16:15");
 let day2 = new WorkDay("06:00", "15:30", "Friday");
 let day3 = new WorkDay("11:55", "20:45");
@@ -124,17 +120,6 @@ let day13 = new WorkDay("06:40", "15:55");
 
 //Generates an array with all the created days
 let days = [day1, day2, day3, day4, day5, day6, day7, day8, day9, day10, day11, day12, day13];
-/*let days = [];
-function pushDays(){
-  for (let i = 0; i< 4; i++){
-    days.push(new WorkDay("06:50", "15:05", "Sunday"));
-    days.push(new WorkDay("06:50", "15:05", "Monday"));
-    days.push(new WorkDay("06:50", "15:05", "Tuesday"));
-    days.push(new WorkDay("06:50", "15:05", "Wednesday"));
-    days.push(new WorkDay("06:50", "15:05", "Thursday"));
-    days.push(new WorkDay("05:59", "15:27", "Friday"));
-  }
 
-pushDays();*/
 document.getElementById("javascript").innerHTML =
-calculateWage(days);
+  calculateWage(days);
